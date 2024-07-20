d9708fc1758a2e52de8e357b9f396a7b5eefc732
Amosgantian <97826100+Amosgantian@users.noreply.github.com>
2024-07-20 18:30:36 +0800

    Update check_vless.sh

--- vless/check_vless.sh
+++ vless/check_vless.sh
@@ -18,7 +18,7 @@ output_yi_xiu() {
     echo -e "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |"
     echo -e " '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ${NC}"
     echo -e "--------------------------------------------------------------------------------------------------"
-    echo -e "一休YouTuBe: ${GREEN}https://www.youtube.com/@yixiu001${NC}"
+    echo -e "YouTuBe: ${GREEN}https://www.youtube.com/@yixiu001${NC}"
     echo -e "TG技术交流群: ${GREEN}https://t.me/yxjsjl${NC}"
     echo -e "--------------------------------------------------------------------------------------------------"
 }
@@ -36,16 +36,16 @@ generate_uuid() {
 
     # 预定义的UUID列表
     predefined_uuids=(
-        "fb210b24-46dd-4b4c-92ce-097385945dad"
-        "53cfcb07-8c25-4c25-baaa-95b4b50871a2"
-        "445ae56f-727d-495e-9c88-cbe942d144a6"
-        "078eb39d-2094-4272-b221-782ba0520dd6"
-        "5826e9cc-c5b7-49ca-8b37-a0ea68f382cc"
-        "e79fda4a-9519-4ef3-8973-130801b3d0ae"
-        "c0422b3b-00aa-4dbe-8573-6fb15d49e557"
-        "907e3ac9-02de-47fe-b40c-c2bd912c3adf"
-        "c53ca34c-8d9c-4a7e-8b44-5da52e4b5eaa"
-        "73fc0a2d-2458-435b-92aa-b4e8e3e40944"
+        "0b03f6c1-a2a1-4e13-d7f3-0722d969235d"
+        "fec4f7ed-5fe7-6cd2-1382-eae506b32db4"
+        "a01f2275-e6ec-2561-708d-b0486854d217"
+        "625f0133-3144-3019-5561-f0cf8ba3742d"
+        "23d000c5-a742-faba-19dc-4ec119eb2985"
+        "bdb70933-3f2f-a61f-6b04-934dab56121f"
+        "f2aa7f67-728f-e7a2-a606-ae94f6806207"
+        "3dea3dfe-f301-552d-d9f4-6dd03aa8949c"
+        "fd4d7d83-1fbc-5dc6-402d-a66052b751a0"
+        "a0802e69-b7a1-133d-644e-d6e30f61aa1b"
     )
     uuid=${predefined_uuids[$RANDOM % ${#predefined_uuids[@]}]}
     echo "$uuid"
